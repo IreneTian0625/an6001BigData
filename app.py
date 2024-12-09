e01f89e76785d1ff6b42656f24c77a3a191f3e8f
IreneTian0625 <IreneT0625@GMAIL.COM>
2024-12-09 11:40:36 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template,request
+import textblob
 
 app = Flask('__name__')
 
@@ -11,6 +12,10 @@ def index():
 def main():
     name = request.form.get('q')
     return(render_template("main.html"))
+
+@app.route('/SA',methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
 
 if __name__=="__main__":
     app.run()
